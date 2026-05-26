c2a30245afe2fa9b8ec5afdb7a2f07c63fa65065
Max <mangoistired@gmail.com>
2026-05-25 22:33:13 -0300

    comecei ai o construtor do obra

--- src/models/Obra.java
+++ src/models/Obra.java
@@ -1,5 +1,18 @@
 package models;
 
+import java.util.Vector;
+
 public class Obra {
+    private String titulo;
+    private String autor;
+    private boolean ativa;
+    private Vector<String> avaliacoes;
+    
+    public Obra(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ativa = true;
+        this.avaliacoes = new Vector<>();
+    }
     
 }
